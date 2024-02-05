5b2c23a24475da61ca48da49ded5630cf5959393
natalykozhevnikova <114256596+natalykozhevnikova@users.noreply.github.com>
2024-02-05 15:47:59 +0300

    Delete pythonProject1 directory

--- pythonProject1/myproject/myapp4/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Myapp4Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myapp4'
--- pythonProject1/myproject/myapp4/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import user_form
-
-
-urlpatterns = [
-    path('user/add/', user_form, name='user_form'),
-]
